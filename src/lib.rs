e222742eba1a7e9bf0d529edf1c180b64c494741
Yuzhao Shi <frank20050415@sjtu.edu.cn>
2025-12-23 19:50:56 +0800

    feat: basic endpoint

---
+++ src/lib.rs
@@ -0,0 +1,26 @@
+use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, dev::Server, web};
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = match req.match_info().get("name") {
+        Some(n) => format!("Hello, {}!", n),
+        None => format!("Hello, world!"),
+    };
+    name
+}
+
+async fn health_check(_req: HttpRequest) -> impl Responder{
+    HttpResponse::Ok().finish()
+}
+
+pub fn run() -> Result<actix_web::dev::Server,std::io::Error> {
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/health_check", web::get().to(health_check))
+            .route("/{name}", web::get().to(greet))
+    })
+    .bind("127.0.0.1:8000")?
+    .run();
+
+    Ok(server)    
+}
